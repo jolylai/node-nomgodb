9c3828ce7de994c0f680dcd7d3f0d83a08753748
jolylai <2635850288@qq.com>
2017-02-14 11:35:06 +0800

    my movie

---
+++ app.js
@@ -0,0 +1,130 @@
+var express = require('express')
+var path = require('path')
+var port = process.env.PORT || 3000
+var app = express()
+var mongoose = require('mongoose')
+var Movie = require('./models/movie')
+var _ = require('underscore')
+// var serveStatic = require('serve-static')
+// var bodyParser = require('body-parser')
+mongoose.connect('mongodb://localhost:27017/imooc')
+mongoose.Promise = global.Promise
+// app.use(bodyParser.urlencoded())
+// app.use(serveStatic('bower_components')
+app.locals.moment =require('moment')
+app.set('views','./views/pages')
+app.set('view engine','jade')
+// 提交表单 数据格式化
+// app.use(express.bodyParser())
+app.use(express.static(path.join(__dirname,'bower_components')))
+app.listen(port)
+
+console.log('started on port' + port)
+
+// 配置路由
+// inde page
+app.get('/',function (req,res) {
+	Movie.fetch(function (err,movies) {
+		if (err) {
+			console.log(err)
+		}
+		res.render('index',{
+			title: '电影首页',
+			movies: movies
+		})
+	})
+})
+
+// detail page
+app.get('/movie/:id',function (req,res) {
+	// 取得id号
+	var id = req.params.id
+	Movie.findById(id,function (err,movie) {
+		res.render('detail',{
+			title: movie.title + '详情页',
+			movie: movie
+		})
+	})
+})
+// admin update movie
+app.get('/admin/update/:id',function (req,res) {
+	var id =req.params.id
+	if (id) {
+		Movie.findById(id,function (err,movie) {
+			res.render('admin',{
+				title: '电影后台更新页',
+				movie:movie
+			})
+		})
+	}
+})
+// admin post movie 提交页面
+app.post('/admin/movie/new',function (req,res) {
+	var id = req.body.movie._id 
+	var movieObj =req.body.movie
+	var _movie
+
+	if (id  !== 'undefind') {
+		Movie.findById(id,function (err,movie) {
+			if (err) {
+				console.log(err)
+			}
+			_movie = _.extend(movie,movieObj)
+			_movie.save(function (err,movie) {
+				if (err) {
+					console.log(err)
+				}
+				res.redirect('/movie/'+movie._id)
+			})
+		})
+	}
+	else{
+		_movie =new Movie({
+			doctor: movieObj.doctor,
+			title: movieObj.title,
+			country: movieObj.country,
+			language: movieObj.language,
+			year: movieObj.year,
+			poster: movieObj.poster,
+			flash: movieObj.flash,
+			summary: movieObj.summary
+		})
+		_movie.save(function (err,movie) {
+			if (err) {
+				console.log(err)
+			}
+			res.redirect('/movie/' + movie._id)
+		})
+	}
+	
+})
+
+// admin page
+app.get('/admin/movie',function (req,res) {
+	res.render('admin',{
+		title: '后台录入页',
+		movie: {
+			title:'',
+			doctor:'',
+			country: '',
+			year:'',
+			poster:'',
+			flash:'',
+			summary:'',
+			language:''
+		}
+	})
+})
+// list page
+app.get('/admin/list',function (req,res) {
+	Movie.fetch(function (err,movies) {
+		if (err) {
+			console.log(err)
+		}
+		res.render('list',{
+			title: '列表页',
+			movies: movies
+		})
+	})
+})
+		
---
+++ schemas/movies.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose')
+var MovieSchema = new mongoose.Schema({
+	doctor: String,
+	title: String,
+	language: String,
+	country: String,
+	summary: String,
+	flash: String,
+	poster: String,
+	year: String,
+	meta: {
+		createAt:{
+			type:Date,
+			default: Date.now()
+		},
+		updateAt: {
+			type:Date,
+			default: Date.now()
+		}
+	}
+})
+
+MovieSchema.pre('save',function (next) {
+	if (this.isNew) {
+		// 如果数据是新建的
+		this.meta.createAt = this.mata.updateAt = Date.now()
+	}
+	else{
+		this.mata.updateAt = Date.now()
+	}
+	next()
+})
+MovieSchema.statics = {
+	fetch: function (cb) {
+		return this
+			.find({})
+			.sort('meta.updateAt')
+			.exec(cb)
+	},
+	findById: function (id,cb) {
+		return this
+			.findOne({_id: id})
+			.exec(cb)
+	}
+}
+module.exports = MovieSchema
